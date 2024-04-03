629542e7b859b9321c51a4d1b015fa24a08df892
mozso <126395568+m0zs0@users.noreply.github.com>
2024-04-03 18:54:00 +0200

    exchangeRateService.getExchangeRate kész

--- src/exchangeRateService.ts
+++ src/exchangeRateService.ts
@@ -1,7 +1,25 @@
 export class IExchangeRateService{
+    public getExchangeRate(fromCurrency: string, toCurrency: string): number {
+        const exchangeRates: { [key: string]: { [key: string]: number } } = {
+            "USD": {
+                "EUR": 0.85,
+                "HUF": 311.72
+            },
+            "EUR": {
+                "USD": 1.18,
+                "HUF": 365.27
+            },
+            "HUF": {
+                "USD": 0.0032,
+                "EUR": 0.0027
+            }
+        };
 
-    public getExchangeRate(fromCurrency: string, toCurrency: string) {
-        return 1;
+        if (exchangeRates[fromCurrency] && exchangeRates[fromCurrency][toCurrency]) {
+            return exchangeRates[fromCurrency][toCurrency];
+        } else {
+            throw new Error("Exchange rate not found.");
+        }
     }
 
 }
